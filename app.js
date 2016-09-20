d8708f7922e6f4a209a0e500dd057de6eaa870a4
aifos710 <sofia.m.caballero@gmail.com>
2016-09-20 10:43:57 -0500

    sub2

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
-window.onload = function(){
-	var boton = document.getElementById("boton"); //boton
-	boton.addEventListener("click", function(){
-		var status = document.getElementById("status").value; //input
-	    var newItem = document.createElement("div"); //para crear nuevo div
-	    var textnode = document.createTextNode(status); //agregar text que esta en var status
-	    newItem.appendChild(textnode); //para agregar
+window.addEventListener("load", function(){
 
-	    var posted = document.getElementById("posted"); // llamando al div general el q ya esta en html
-	    posted.insertBefore(newItem, posted.childNodes[0]); // para insertar antes
+	var boton = document.getElementById("boton"); 
+
+	boton.addEventListener("click", function(e){
+		e.preventDefault(); 
+		var textarea = document.getElementById("status");
+		var status = textarea.value;
+		agregarMensaje(status);
+		document.getElementById("count").innerText = "140"
+		boton.disabled = true;
+		textarea.value ="";
 	});
 
-}
+	function agregarMensaje(status) {
+		var div = document.createElement("div");
+		div.innerText = status;
+		var posted = document.getElementById("posted");
+		posted.insertBefore(div, posted.childNodes[0]).classList.add("box");
+	}
+
+	var status = document.getElementById("status");
+	status.addEventListener("keyup", function() {
+		var maxcharacter = 140;
+		var len = document.getElementById("status").value.length;
+
+		if(len <= maxcharacter) {
+			document.getElementById("count").innerText = maxcharacter - len;	
+		} else {
+			document.getElementById("count").innerText = maxcharacter - len;
+		}
+
+		if(this.value.length > 0)
+			boton.disabled = false;
+		else
+			boton.disabled =true;
+	});
+
+});
+
+
 
 
 
